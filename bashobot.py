0d93244c2edc006e1ecf3b816cf63b3fd3c4d879
Stephen Fay <dcxstephen@live.co.uk>
2022-04-10 20:37:41 -0400

    bashobot.py refactored

--- bashobot.py
+++ bashobot.py
@@ -7,6 +7,8 @@ from datetime import date
 import os
 import random
 
+ROOT = "/Users/steve/Documents/code/bashobot/"
+
 def format_post(haiku , options=None):
     haiku = " // ".join(haiku.split("\n"))
     selftext = ""
@@ -40,15 +42,15 @@ def find_valid_and_load(haiku_files):                   # arg is a list of haiku
 ### Main script
 if __name__=="__main__":
     ### Make sure no poems have been submitted today
-    with open("/Users/steve/Documents/code/bashobot/date_last_posted.txt","r") as f:
+    with open(ROOT + "date_last_posted.txt","r") as f:
         date_last_posted = f.read()                     # the date a haiku was last posted by this script 
         today_str = f"{date.today()}"                   # today's date formatted as a string
         assert today_str != date_last_posted , "This script was already used today"
 
 
     ### API credentials and object initialization
-    credentials_fname = 'client_secrets.json'           # api credential file
-    with open(credentials_fname) as f:                  # open the credentials file
+    credentials_path = ROOT + 'client_secrets.json'     # api credential file
+    with open(credentials_path) as f:                   # open the credentials file
         creds = json.load(f)
     reddit = praw.Reddit(client_id=creds['client_id'],  # init a reddit api object
             client_secret=creds['client_secret'],
@@ -60,39 +62,37 @@ if __name__=="__main__":
 
 
     ### Randomly iterate through haiku data files until you find one that has haiku in it that haven't yet been posted by this script
-    haiku_files = ["./data/" + i for i in os.listdir("./data") if i[-5:] == ".json"] 
+    haiku_files = [ROOT + "data/" + fname for fname in os.listdir("./data") if i[-5:] == ".json"] 
     random.shuffle(haiku_files)                         # shuffle the list
     haiku_dict , path_to_json = find_valid_and_load(haiku_files) # load the source json haiku into a python dictionary
-    opt_fields = list(haiku_dict.keys())                # these are the options that can be passed as metadata, e.g. author
-    opt_fields.remove("posted_haikus") 
-    opt_fields.remove("haikus") 
 
-    options = {}
-    for field in opt_fields:
-        options[field] = haiku_dict[field]              # assume all other fields are all right
+    opt_fields = list(haiku_dict.keys())                # these are the options that can be passed as metadata, e.g. author
+    opt_fields.remove("posted_haikus")                  # remove the fields that are not the haiku (i.e. everything but the options fields)
+    opt_fields.remove("haikus")                         # remove the fields that are not the haiku (i.e. everything but the options fields) 
+    options = {}                                        # init options dict
+    for field in opt_fields:                            
+        options[field] = haiku_dict[field]              # assumes all other fields are all right
     
 
     ### load a random haiku and comment into the title and selftext fields respectively
-    haiku_idx = random.randrange(len(haiku_dict["haikus"])) # randomly choose a haiku (index)
-    haiku , selftext = format_post(haiku_dict["haikus"][haiku_idx] , options) # format it
+    haikus = haiku_dict["haikus"]                       # a list
+    idx = random.randrange(len(haikus))           # randomly choose a haiku (index) in this path
+    haiku , selftext = format_post(haikus[idx] , options) # format it
 
     ### Post the haiku
     # print(f"Uncomment the next line to actually post;\ntitle={haiku}\nselftext={selftext}")
     # assert False                                        # forcefully end the program, for testing purposes
     traditional_haiku.submit(haiku, selftext=selftext)
+    print(f"\n\nPosted\n{haikus[idx]}")
 
     ### Update last_posted.txt which contains the date of the last poem that was posted using this script
     with open("date_last_posted.txt","w") as f:
         f.write(f"{date.today()}")
 
     ### It is posted successfully, update json data 
-    haiku_dict["posted_haikus"].append(haiku_dict["haikus"][haiku_idx])
-    haiku_dict["haikus"] = haiku_dict["haikus"][:haiku_idx] + haiku_dict["haikus"][haiku_idx+1:]
+    haiku_dict["posted_haikus"].append(haikus[idx])
+    haiku_dict["haikus"] = haikus[:idx] + haikus[idx+1:]
     with open(path_to_json , "w") as f:
         json.dump(haiku_dict , f)
     
-
-
-
-
 
